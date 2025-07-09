6fcb320a6f42c03da8dac415cf52acc9e728210a
Emiraldo Lamkja <Elamkia@gmail.com>
2025-07-09 16:06:52 +0300

    Starting

---
+++ TwoSum/FasterSolution.php
@@ -0,0 +1,19 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param int[] $nums
+     * @param int $target
+     * @return int[]
+     */
+    function twoSum(array $nums, $target)
+    {
+        $hashmap = array();
+        for($i=0;$i<count($nums);$i++)
+        {
+        
+        }
+    }
+}
