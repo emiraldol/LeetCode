75d7c0a7ae1be948aa2fe7d08028b2f6807da951
Emiraldo Lamkja <Elamkia@gmail.com>
2025-07-09 16:47:04 +0300

    Finished Two Sum

--- TwoSum/FasterSolution.php
+++ TwoSum/FasterSolution.php
@@ -11,9 +11,21 @@ class Solution
     function twoSum(array $nums, $target)
     {
         $hashmap = array();
-        for($i=0;$i<count($nums);$i++)
-        {
-        
+        for ($i = 0; $i < count($nums); $i++) {
+            $remain = $target - $nums[$i];
+            if (array_key_exists($remain, $hashmap)) {
+                $index_0 = $hashmap[$remain];
+                $index_1 = $i;
+
+                return array($index_0, $index_1);
+            }
+            $hashmap[$nums[$i]] = $i;
         }
     }
 }
+
+
+$test = new Solution();
+$array = array(3, 3);
+$wow = $test->twoSum($array, 6);
+print_r($wow);
